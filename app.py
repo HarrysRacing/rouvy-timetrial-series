a4f5a604e714df8753adb01d996cfe8c9eeaa857
HarrysRacing <88748083+HarrysRacing@users.noreply.github.com>
2026-07-10 07:38:56 -0400

    Changed to run using Flask

--- app.py
+++ app.py
@@ -1,158 +1,42 @@
-import os
-import secrets
-import urllib.parse
-import requests
+# import the Flask library
+from flask import Flask, render_template
 
-from flask import Flask, redirect, request
-from dotenv import load_dotenv
-
-load_dotenv()
-
+# app is a standard construct in Flask and where app.py is called from a python session "__name__" will be set by python to "__main__"
+# hence further down the app.run will be triggered in debug mode
+# if app.py were to be called as part of, or from, a module "__name__" would equate to the module name and app.run would not trigger
+# Further to this __name__ equates not only to the calling module name, but also the full path, held in teh metadata by python. 
+# So Flask therefore knows the location of the calling module and uses this to locate the relevant folders, such as templates, static etc...
 app = Flask(__name__)
 
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-REDIRECT_URI = os.getenv("REDIRECT_URI")
-
-AUTH_URL = "https://api.rouvy.com/oauth/authorize"
-
-TOKEN_URL = "https://api.rouvy.com/oauth/token"
-
-
+# app.route() is a standard flask decorator function that works as:
+# when the root "/" URL is entered in the browser, app.route("/") matches this and calls the home() function that returns/renders index.html 
+# which flask will look for in the standard templates folder
+#note the "home" function could in this case be named anything you choose
 @app.route("/")
 def home():
-    return '<a href="/login">Login with ROUVY</a>'
+    return render_template("index.html")
 
-
-@app.route("/login")
-def login():
-
-    state = secrets.token_hex(16)
-
-    params = {
-        "client_id": CLIENT_ID,
-        "redirect_uri": REDIRECT_URI,
-        "scope": "profile:read activities:read events:read",
-        "response_type": "code",
-        "state": state
-    }
-
-    url = AUTH_URL + "?" + urllib.parse.urlencode(params)
-
-    return redirect(url)
-
-
-@app.route("/auth/callback")
-def callback():
-
-    code = request.args.get("code")
-
-    token_response = requests.post(
-        TOKEN_URL,
-        data={
-            "grant_type": "authorization_code",
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET,
-            "code": code,
-            "redirect_uri": REDIRECT_URI
-        }
-    )
-
-    tokens = token_response.json()
-
-    print("TOKENS:")
-    print(tokens)
+@app.route("/index")
+def index():
+    return render_template("index.html")
     
-    access_token = tokens["access_token"]
-
-    # GET ME Profile
-    """  
-    profile_response = requests.get(
-        "https://api.rouvy.com/me",
-        headers={
-            "Authorization": f"Bearer {access_token}"
-        }
-    )
-
-    print("PROFILE:")
-    print(profile_response.json())
-    """
+@app.route("/guide")
+def guide():
+    return render_template("guide.html")
     
-    """
-    #GET Event Details ME Owns : Pulls Event_ID
-    event_response = requests.get(
-      "https://api.rouvy.com/me/events",
-      headers={
-          "Authorization": f"Bearer {access_token}"
-        },
-        params={
-          "role": "owner",
-          "status": "offline",
-          "startDateTimeUtcFrom": "",
-          "startDateTimeUtcTo": "",
-          "limit": "100",
-          "offset": "0"
-        }
-    )
+@app.route("/about")
+def about():
+    return render_template("about.html")   
 
-    print("EVENT Details:")
-    print(event_response.json())  
-    print("EVENT Deatils END")
-    """
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")  
 
-    # GET Event Details using Event_ID
-    """
-    event_response = requests.get(
-      "https://api.rouvy.com/events/4ff690eb-ccbc-48f2-92b0-d5f1176b323c",
-      headers={
-          "Authorization": f"Bearer {access_token}"
-        }
-    )
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")    
 
-    print("EVENT:")
-    print(event_response.json())   
-    """
-    
-    """
-    # GET Event Startlist using Event_ID
-    event_response = requests.get(
-      "https://api.rouvy.com/events/4ff690eb-ccbc-48f2-92b0-d5f1176b323c/startlist",
-      headers={
-          "Authorization": f"Bearer {access_token}"
-        },
-        params={
-          "limit": "100",
-          "offset": "0"
-        }
-    )
-    
-    print("EVENT Startlist:")
-    print(event_response.json())  
-    print("EVENT Startlist END")
-    """
-    
-    # GET Event Activities using Event_ID  
-    event_response = requests.get(
-      "https://api.rouvy.com/events/4ff690eb-ccbc-48f2-92b0-d5f1176b323c/activities",
-      headers={
-          "Authorization": f"Bearer {access_token}"
-        },
-        params={
-           "limit": "100",
-           "offset": "0"
-        }
-    )
-   
-    print("EVENT Activities:")
-    print(event_response.json())  
-    print("EVENT Activities END")   
-    
-    return 
-    """
-    <h1>Authorization successful</h1>
-    <p>Check terminal for tokens.</p>
-    """
-
-
+# Start with flask web app, with debug as True,
+# only if this is the starting page
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(debug=True)
